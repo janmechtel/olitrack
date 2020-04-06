6fe946d0dd57b6d72151d76ec8dba34e7239addc
Jan Mechtel <jme@templafy.com>
2020-04-06 20:25:25 +0200

    passing symbols as parameters

--- web/src/components/PricesCell/PricesCell.js
+++ web/src/components/PricesCell/PricesCell.js
@@ -2,8 +2,8 @@ import {Line as LineChart} from 'react-chartjs-2';
 import pricesSymbol from 'src/'
 
 export const QUERY = gql`
-  query {
-    prices ( symbols: ["EXSA.XETRA","DX2J.XETRA","SXRG.XETRA"]) {
+  query($symbols: [String!]) {
+    prices (symbols: $symbols) {
       symbol_exchange
       prices {
         id
@@ -21,60 +21,62 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ prices }) => {
+export const Success = ({ prices, symbols }) => {
   console.log(prices)
   const datasets = []
 
   prices.forEach((price,index) => {
-    // console.log(price)
-    const data = []
-    const firstClose = price.prices[0].value
+    if (price.prices.length>0) {
+      // console.log(price)
+      const data = []
+      const firstClose = price.prices[0].value
 
-    price.prices.forEach(p => {
-      if (p.value >0) {
-        data.push({
-          x: p.date,
-          y: p.value/firstClose
-        })
+      price.prices.forEach(p => {
+        if (p.value >0) {
+          data.push({
+            x: p.date,
+            y: p.value/firstClose
+          })
+        }
+      });
+      console.log(index);
+
+      var color = "green"
+      switch (index) {
+        case 0:
+          color="red"
+          break;
+        case 1:
+          color="black"
+          break;
+        case 2:
+          color="blue"
+          break
+        case 3:
+          color="yellow"
+          break
+        case 3:
+          color="orange"
+          break
+        default:
+          break
       }
-    });
-    console.log(index);
 
-    var color = "green"
-    switch (index) {
-      case 0:
-        color="red"
-        break;
-      case 1:
-        color="black"
-        break;
-      case 2:
-        color="blue"
-        break
-      case 3:
-        color="yellow"
-        break
-      case 3:
-        color="orange"
-        break
-      default:
-        break
+
+      datasets.push(
+        {
+          label: price.symbol_exchange,
+          fill: false,
+          pointHoverRadius: 5,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: data,
+          spanGaps: false,
+          borderColor: color
+        }
+
+      )
     }
-
-
-    datasets.push(
-      {
-        label: price.symbol_exchange,
-        fill: false,
-        pointHoverRadius: 5,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: data,
-        spanGaps: false,
-        borderColor: color
-      }
-
-    )
   });
 
   const chartData = {
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -4,29 +4,29 @@ import PricesCell from 'src/components/PricesCell';
 import React, { useEffect, useState } from 'react';
 
 //TODO find out how to get query parameters from React/Router?
-// const query =document.location.search;
-// var symbols = []
-// if (query) {
-//   (query.split("?")[1].split("&")).forEach(function(p) {
-//     if (p!="") {
-//       var splits = p.split("=")
-//       switch (splits[0]) {
-//         case 'symbols':
-//           symbols = splits[1].split(",")
-//           break;
-//         default:
-//           break;
-//       }
-//     }
-//   })
-//   console.log(symbols)
-// }
+const query =document.location.search;
+var symbols = []
+if (query) {
+  (query.split("?")[1].split("&")).forEach(function(p) {
+    if (p!="") {
+      var splits = p.split("=")
+      switch (splits[0]) {
+        case 'symbols':
+          symbols = splits[1].split(",")
+          break;
+        default:
+          break;
+      }
+    }
+  })
+  console.log(symbols)
+}
 
 const HomePage = () => {
 
   return (
     <div>
-      <PricesCell />
+      <PricesCell symbols={symbols}/>
     </div>
   )
 }
