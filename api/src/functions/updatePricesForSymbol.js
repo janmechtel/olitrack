44578d0adacfcc507e1aebeb861387285f2e5c67
janmechtel <jmechtel@gmail.com>
2020-04-12 20:34:06 +0200

    switching to async forEach

--- api/src/functions/updatePricesForSymbol.js
+++ api/src/functions/updatePricesForSymbol.js
@@ -1,29 +1,29 @@
 import { db } from 'src/lib/db'
 import fetch from 'node-fetch'
 
-export const savePrices = (symbol_exchange, prices) => {
+const savePrices = async (symbol_exchange, prices) => {
   console.log("Saving new price data for " + symbol_exchange)
-  prices.forEach(price => {
+  for (let index = 0; index < prices.length; index++) {
     try {
       var price = await db.price.create ({data: {
         symbol_exchange: symbol_exchange,
-        date: new Date(price.date),
-        value:  Math.trunc(Number(price.adjusted_close*100))
+        date: new Date(prices[index].date),
+        value:  Math.trunc(Number(prices[index].adjusted_close*100))
       }})
       console.log(price)
-
     } catch(error) {
         console.log(error)
     }
-  });
+  }
   // console.log(prices)
   return {result:"Success"}
 }
 
 exports.handler = async (event, context) => {
+  console.log("test")
   const symbol_exchange = event.queryStringParameters.symbol;
   const from = event.queryStringParameters.from;
-  console.log(from + symbol_exchange)
+  console.log(from + "-" + symbol_exchange)
 
   const url = "https://eodhistoricaldata.com/api/eod/AAPL.US?from="+from+"-1&api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX&period=d&fmt=json"
   // const url = "https://eodhistoricaldata.com/api/eod/"+symbol_exchange+"?from="+event.queryStringParameters.from+"&api_token=5e89e1d8bc5662.72708807&period=d&fmt=json"
@@ -32,7 +32,8 @@ exports.handler = async (event, context) => {
   var response = await fetch(url)
   console.log(response)
   var json = await response.json()
-  console.log(savePrices(symbol_exchange,json))
+  await savePrices(symbol_exchange,json)
+  console.log("done with updating")
 
   db.disconnect()
   return {
